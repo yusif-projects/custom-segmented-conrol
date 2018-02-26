2d5bb8ff106eda8cd8d88fdef22c3350110ac164
Yusif Aliyev <yusif.projects@gmail.com>
2018-02-26 23:22:04 +0400

    Cleaned-up the project from the default comments, created groups and created "Custom_Segmented_Control" class file.

---
+++ Custom Segmented Conrol/Controller/Main_VC.swift
@@ -0,0 +1,12 @@
+// Created by Yusif Aliyev
+// February 26, 2018
+
+import UIKit
+
+class Main_VC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
---
+++ Custom Segmented Conrol/View/Custom_Segmented_Control.swift
@@ -0,0 +1,16 @@
+// Created by Yusif Aliyev
+// February 26, 2018
+
+import UIKit
+
+class Custom_Segmented_Control: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
